9aab792909c8c8236e266a7f60d992e612acf4ab
benjisqt <128649752+benjisqt@users.noreply.github.com>
2023-10-19 01:36:43 +0100

    🚀 new/stable: added raid protection!

--- src/commands/Utility/RaidProtection.ts
+++ src/commands/Utility/RaidProtection.ts
@@ -2,6 +2,7 @@ import { Command } from "../../structures/Command";
 import { ApplicationCommandOptionType, ChannelType } from "discord.js";
 import Antijoin from "../../models/Antijoin";
 import RaidProtection from "../../models/RaidProtection";
+import Reply from "../../functions/Reply";
 
 export default new Command({
     name: 'raid-protection',
@@ -49,17 +50,58 @@ export default new Command({
                     raidprot.save();
                 }
 
-                const channels = await guild.channels.cache.filter((ch) => ch.permissionsFor(client.user).has('ManageChannels') && ch.isTextBased());
+                const channels = await guild.channels.cache.filter((ch) => ch.permissionsFor(guild.members.me).has('ManageChannels') && ch.isTextBased() && ch.permissionsFor(mr.id).has('SendMessages'));
                 if(channels.size <= 0) throw "There are no channels that give me the permission to change their permissions!";
 
+                let allowedchannels = [];
+
                 channels.forEach((cha) => {
-                    if(!cha.isTextBased()) return;
+                    cha.permissionsFor(mr.id).remove('SendMessages');
+                    allowedchannels.push(cha.id);
                 });
+
+                const aj = await Antijoin.findOne({ Guild: guild.id });
+                if(!aj) {
+                    await Antijoin.create({
+                        Guild: guild.id,
+                        BotException: false,
+                        Punishment: 'Ban',
+                    });
+                } else {
+                    aj.Punishment = 'Ban';
+                    aj.save();
+                }
+
+                return Reply(interaction, `Raid protection enabled!\n${allowedchannels.length} channels have been locked.\nAntijoin has been enabled.`, `✅`, false);
             }
             break;
 
             case 'disable': {
+                const raidprot = await RaidProtection.findOne({ Guild: guild.id });
+                if(!raidprot) {
+                    throw "The raid protection isn't enabled!";
+                } else {
+                    if(raidprot.Enabled === false) throw "Raid Protection is already disabled!";
+                    raidprot.Enabled = false;
+                    raidprot.save();
+                }
 
+                const channels = await guild.channels.cache.filter((ch) => ch.permissionsFor(guild.members.me).has('ManageChannels') && ch.isTextBased() && ch.permissionsFor(mr.id).has('SendMessages'));
+                if(channels.size <= 0) throw "There are no channels that give me the permission to change their permissions!";
+
+                let allowedchannels = [];
+
+                channels.forEach((cha) => {
+                    cha.permissionsFor(mr.id).add('SendMessages');
+                    allowedchannels.push(cha.id);
+                });
+
+                const aj = await Antijoin.findOne({ Guild: guild.id });
+                if(aj) {
+                    await aj.deleteOne({ new: true });
+                }
+
+                return Reply(interaction, `Raid protection disabled!\n${allowedchannels.length} channels have been unlocked.\nAntijoin has been disabled.`, `✅`, false);
             }
             break;
         }
